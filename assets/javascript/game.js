6f89abaf081724f76f29ea4b0462800ebacdad66
JosephRivera10 <joseph.rivera.09@gmail.com>
2018-01-25 23:10:53 -0600

    added music

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -25,6 +25,7 @@
 		counterAttack: 13,
 		photo: "assets/images/gandalf.jpg",
 		gif: "https://media.giphy.com/media/P726XW1pK3Luo/giphy.gif",
+		audioFile: "assets/sound/shallnotpass.wav"
 	}
 	var orc = {
 		name: "orc",
@@ -48,12 +49,23 @@
 	}
 
 	var characters = [aragorn, gandalf, orc, sauron];
+	
+	var audio = new Audio("assets/sound/theme.mp3");
+
+	var audio1 = new Audio("assets/sound/shallnotpass.wav");
+
+	//var audio = new Audio("assets/sound/shallnotpass.wav");
+	//var audio = $("#mySoundClip")[0];
+	//$("nav a").mouseenter(function() {
+  	//	audio.play();
+	//	});
 
 	$(document).ready(function() {
 
 	$("#result").html("Lord of the Rings");
 	//hide new game area
 	$(".newGame").hide();
+	audio.play();
 
 	$(".playerCharPick").on("click", function() {
 		//reads data obj for character name
@@ -74,9 +86,9 @@
 				$("#enemyCharSelection").append('<div class = "col-md-3 cont"><img src="' + characters[i].photo + '" class="enemyCharPick" data-obj="' + characters[i].name + '"></div>');
 			}
 		}
-
 	});
-		//click for enemy
+
+			//click for enemy
 	$("#enemyCharSelection").on("click", ".enemyCharPick", function() {
 		//shows button for new game
 		$(".newGame").show();
@@ -101,36 +113,49 @@
 		// player attacks and player being attacks loses health equal to attack
 		// Player attack increases by base amount
 		// If enemy is not dead, enemy counter attacks, player loses health equal to enemy counter attk
+		//checks to see if battling
 		if (enemyBattling == true) {
-
+			//health = health - attack
+			//attack = attack + baseAttack
 			enemyPicked.health -= characterPicked.attack;
 			characterPicked.attack += characterPicked.baseAttack;
+			//hit to health and attack strenght are updated
 			updateEnemyStats();
-
-			if (enemyPicked.health <= 0) { //Checks to see if enemy has been defeated
+			//if enemy's health is less than 0
+			if (enemyPicked.health <= 0) { 
+				//removes enemy
+				//empties name and health
 				$("#enemyChar").remove();
 				$("#enemyName").html("");
 				$("#enemyHealth").html("");
+				//checksif battling
 				enemyBattling = false;
-				if (enemies.length == 3) { //Once all 3 enemies have been fought
+				//checks to see if all enemies are defeated
+				if (enemies.length == 3) { 
+
 					var enemyLiving = false;
+					//goes through enemies and checks for health status. if there is health then enemy living true
 					for (i=0; i<enemies.length;i++) {
 						if (enemies[i].health > 0) {
 							enemyLiving = true;
 						}
 					}
-					if (enemyLiving == false) { //Once all 3 enemies have 0 health
+					// there is no health player wins
+					//result section says player wins
+					//hide attack button
+					if (enemyLiving == false) { 
 						playerWon = true;
 						$("#result").html("Player 1 Wins!");
 						$(".attack").hide();
 					}
 				}
 			}
-
+				//characters health = characters health - counter attack
 			else {
 				characterPicked.health -= enemyPicked.counterAttack;
 				updatePlayerStats();
-					if (characterPicked.health <= 0) { //Checks to see if player has been defeated
+				//if characters health is below 0 then player loses
+					if (characterPicked.health <= 0) { 
 						playerLoss = false;
 						$("#result").html("CPU Wins");
 						$(".attack").hide();
@@ -138,19 +163,22 @@
 			}
 			
 		}
+		//if battling not true then select another enemy
 		else {
 			alert("Please select another enemy");
 		}
 	});
-
+	//click new game initiat reload - reload screen
 	$(".newGame").on("click", function() {
 		location.reload();
 	});
 
+	//function to update stats
 	function updatePlayerStats() {
 		$("#playerHealth").html("HP: " + characterPicked.health + "<br />Attack: " + characterPicked.attack);
 		$("#playerName").html(characterPicked.display);
 	}
+	//function to update enemy stats
 	function updateEnemyStats() {
 		$("#enemyHealth").html("HP: " + enemyPicked.health + "<br />Attack: " + enemyPicked.attack);
 		$("#enemyName").html(enemyPicked.display);
